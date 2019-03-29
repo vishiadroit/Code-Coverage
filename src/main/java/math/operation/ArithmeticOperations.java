c9f580367d9b12e7a6987f970a26c06780415cb5
vishi9 <vishal_sharma0988@outlook.com>
2019-03-29 11:40:57 +1100

    Updated

--- src/main/java/math/operation/ArithmeticOperations.java
+++ src/main/java/math/operation/ArithmeticOperations.java
@@ -11,4 +11,9 @@ public class ArithmeticOperations {
 	{
 		return a-b;
 	}
+	
+	public Integer multiply(Integer a, Integer b)
+	{
+		return a*b;
+	}
 }
--- src/test/java/math/operation/ArithmeticOperationsTest.java
+++ src/test/java/math/operation/ArithmeticOperationsTest.java
@@ -10,10 +10,19 @@ public class ArithmeticOperationsTest {
 	@Test
 	public void testAdd()
 	{
-	
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.add(2, 6);
 		Integer expected = 8;
+		assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testSubtract()
+	{
+		ArithmeticOperations operations = new ArithmeticOperations();
+		Integer actual = operations.subtract(6, 2);
+		Integer expected = 4;
 		assertEquals(expected, actual);
 		
 	}
